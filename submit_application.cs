c890aecac4466ff67cc34fffadc9f4994b8fcf1e
Jason Farrell <imjason@gmail.com>
2018-07-11 21:30:59 -0500

    Gave up on multiple inputs

--- submit_application.cs
+++ submit_application.cs
@@ -8,15 +8,20 @@ using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using System;
 using AccountService.Models;
+using System.Threading.Tasks;
 
 namespace AccountService.Functions
 {
     public static class AccountService
     {
         [FunctionName("submit_application")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]NewUserInfo newUser, [HttpTrigger]AccountApplication applicationData, TraceWriter log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
+            string rawContents = await req.ReadAsStringAsync();
+            NewUserInfo newUser = JsonConvert.DeserializeObject<NewUserInfo>(rawContents);
+            AccountApplication applicationData = JsonConvert.DeserializeObject<AccountApplication>(rawContents);
+
             log.Info($"Username: {newUser.Username}");
             log.Info($"Account Name: {applicationData.AccountName}");
 

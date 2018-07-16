4d4be42a193d3714d85d5419a5ce12486237695b
Jason Farrell <imjason@gmail.com>
2018-07-16 11:43:34 -0500

    updated users database

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -10,8 +10,8 @@ namespace AccountService.Services
         public static async Task<UserInfo> CreateUser(UserInfo newUser)
         {
             var client = new MongoClient("mongodb://financeapp:1e5Q5BuE7wRjGYmPSDj3IHK7gbQifFCvMwx7YoviCrUg88YK1YX3go74vYyeYwlzbrsCOxSfzB8iCVopJ7xHSw==@financeapp.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
-            var database = client.GetDatabase("user_info");
-            var collection = database.GetCollection<UserInfo>("user_data");
+            var database = client.GetDatabase("users");
+            var collection = database.GetCollection<UserInfo>("users");
 
             await collection.InsertOneAsync(newUser);
 
@@ -21,8 +21,8 @@ namespace AccountService.Services
         public static async Task<UserInfo> GetUser(string userId)
         {
             var client = new MongoClient("mongodb://financeapp:1e5Q5BuE7wRjGYmPSDj3IHK7gbQifFCvMwx7YoviCrUg88YK1YX3go74vYyeYwlzbrsCOxSfzB8iCVopJ7xHSw==@financeapp.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
-            var database = client.GetDatabase("user_info");
-            var collection = database.GetCollection<UserInfo>("user_data");
+            var database = client.GetDatabase("users");
+            var collection = database.GetCollection<UserInfo>("users");
 
             var result = await collection.FindAsync(x => x.Id == userId);
             return result.FirstOrDefault();

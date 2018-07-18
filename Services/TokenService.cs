8cb83e180622e6190d73a856632129593c9930fe
Jason Farrell <imjason@gmail.com>
2018-07-18 13:39:06 -0500

    added parsing logic and loggiing for response

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -2,25 +2,28 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json.Linq;
 
 namespace AccountService.Services
 {
     public static class TokenService
     {
-        public static async Task<string> GetUserIdForToken(string token)
+        public static async Task<string> GetUserIdForToken(string token, TraceWriter log)
         {
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync($"https://financeapp-authservice.azurewebsites.net/api/validate");
                 client.DefaultRequestHeaders.Add("auth-key", token);
                 client.DefaultRequestHeaders.Add("x-functions-key", "gpaYFglPWlvPUCfbH/GgZTf9lFO2T48RSY4rQPH19vuQq0wiwQ7OVQ==");
+                log.Info($"status {response.StatusCode.ToString()}");
                 if (response.StatusCode != HttpStatusCode.OK)
                 {
                     return string.Empty;
                 }
 
                 var responseContent = await response.Content.ReadAsStringAsync();
+                log.Info($"content {responseContent}");
                 return JObject.Parse(responseContent)["userId"].Value<string>();
             }
         }
